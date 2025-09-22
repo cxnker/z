e82b1a5ca3f46c10201cf5964735ac666866fe61
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-09-22 11:57:31 -0400

    Update VHub.lua

--- VHub.lua
+++ VHub.lua
@@ -7,7 +7,7 @@ local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterGui = game:GetService("StarterGui")
 
-local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/cxnker/z/refs/heads/main/BRUTONHUBUI.txt"))()
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/cxnker/z/refs/heads/main/TestUi.lua"))()
 local Window = Lib:MakeWindow({
     Title = "Victory Hub | Brookhaven 🌠 ",
     SubTitle = " by Nort_VT",
