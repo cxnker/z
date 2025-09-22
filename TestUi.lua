83acad5fcd6481576f672a35f842c5dee0868162
Cxnker <72629137+cxnker@users.noreply.github.com>
2025-09-22 13:24:21 -0400

    Update TestUi.lua

--- TestUi.lua
+++ TestUi.lua
@@ -17,7 +17,7 @@ local redzlib = {
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 128, 255))
 			}),
 			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-			["Color Stroke"] = Color3.fromRGB(5, 5, 5),
+			["Color Stroke"] = Color3.fromRGB(20, 20, 20),
 			["Color Theme"] = Color3.fromRGB(0, 128, 255),
 			["Color Text"] = Color3.fromRGB(180, 180, 180),
 			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
